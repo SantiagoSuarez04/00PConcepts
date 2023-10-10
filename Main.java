e21819dae0f96c168559b0f72622dc876b2d9fd4
sis <sisoy>
2023-10-09 22:06:41 -0500

    sisoy

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+public class Main {
+    public static final int color_amarillo = new Color(250,201,20).getRGB();
+    public static final int color_azul = new Color(17,13,99).getRGB();
+    public static final int color_rojo = new Color(196,0,15).getRGB();
+    public static final int color_negro = new Color(5,5,5).getRGB();
+    public static final int color_blanco = new Color(255,255,255).getRGB();
+    public static final int color_verde = new Color(35,138,51).getRGB();
+    public static void main(String[] args) {
+
+    }
+}
